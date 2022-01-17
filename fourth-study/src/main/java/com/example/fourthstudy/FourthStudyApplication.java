4c67dbf38c386d8a8669e706fe8ddb6c25ae6360
kaankalan <kaankalan@outlook.com.tr>
2022-01-17 23:23:39 +0300

    Initial commit

---
+++ fourth-study/src/main/java/com/example/fourthstudy/FourthStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.fourthstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourthStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourthStudyApplication.class, args);
+    }
+
+}
